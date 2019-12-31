8b4fa31dbb8b0f8fba11bf6f31ee09897e9da8ee
vladmog <vladmog@gmail.com>
2019-12-31 15:47:21 -0800

    Markdown pages now creating pages with custom URLs

--- vmportfolio/src/pages/index.js
+++ vmportfolio/src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import { graphql } from "gatsby"
 
 import Quadrant1 from "../components/landing/quadrant1";
 import Quadrant2 from "../components/landing/quadrant2";
@@ -7,6 +8,7 @@ import Quadrant3 from "../components/landing/quadrant3";
 import Quadrant4 from "../components/landing/quadrant4";
 
 import Layout from "../components/layout";
+import PostLink from "../components/post-link"
 
 const S = {};
 
@@ -31,21 +33,63 @@ S.Container = styled.div`
     
 `
 
+const Landing = ({
+    data: {
+      allMarkdownRemark: { edges },
+    },
+  }) => {
+    const Posts = edges
+      .filter(edge => !!edge.node.frontmatter.date) // You can filter your posts based on some criteria
+      .map(edge => <PostLink key={edge.node.id} post={edge.node} />)
+    return (
+        <Layout>
+            <S.Container>
+                <div>{Posts}</div>
+                <Quadrant1 />
+                <Quadrant2 />
+                <Quadrant3 />
+                <Quadrant4 />  
+                <footer>
+                    Icons made by Freepik from www.flaticon.com
+                </footer>  
+            </S.Container>
+        </Layout>
+    )
+  }
 
 
-const Landing = (props) => (
-  <Layout>
-    <S.Container>
-        <Quadrant1 />
-        <Quadrant2 />
-        <Quadrant3 />
-        <Quadrant4 />  
-        <footer>
-            Icons made by Freepik from www.flaticon.com
-        </footer>  
-    </S.Container>
-  </Layout>
-)
+
+// const Landing = (props) => (
+//   <Layout>
+//     <S.Container>
+//         <Quadrant1 />
+//         <Quadrant2 />
+//         <Quadrant3 />
+//         <Quadrant4 />  
+//         <footer>
+//             Icons made by Freepik from www.flaticon.com
+//         </footer>  
+//     </S.Container>
+//   </Layout>
+// )
 
 
 export default Landing;
+
+export const pageQuery = graphql`
+  query {
+    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
+      edges {
+        node {
+          id
+          excerpt(pruneLength: 250)
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            path
+            title
+          }
+        }
+      }
+    }
+  }
+`
