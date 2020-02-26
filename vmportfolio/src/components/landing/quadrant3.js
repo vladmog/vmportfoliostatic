12b182027f83e798ef6bb14477bff1b224af6a08
vladmog <vladmog@gmail.com>
2020-02-26 00:22:40 -0800

    Re-ordered selected works

--- vmportfolio/src/components/landing/quadrant3.js
+++ vmportfolio/src/components/landing/quadrant3.js
@@ -50,25 +50,6 @@ S.Block = styled.div`
 
 const selectedWorks = [
     {
-        name: "Price My Airbnb",
-        body: "PriceMyAirbnb is a software solution to the problem of properly pricing Airbnb listings. Using a database of United-States AirBnb listings and data science calculations, the app lays out the financial landscape of users’ locations. This allows them to see where they stand and make informed choices on their properties.",
-        techStack: [
-            "React JS",
-            "React-Router",
-            "Axios",
-            "Chart JS",
-            "Auth0",
-            "styled-components",
-            "Node JS",
-            "Express",
-            "Knex",
-            "Postgresql"
-        ],
-        beGithub: "https://github.com/Lambda-School-Labs/airbnb-unit-price-analysis-be",
-        feGithub: "https://github.com/Lambda-School-Labs/airbnb-unit-price-analysis-fe",
-        linkUrl: "https://pricemyairbnb.netlify.com/"
-    },
-    {
         name: "Tagger",
         body: "An email client whose data-science-infused search returns emails to queries without the need for exact word matches. As the team lead, solely responsible for coordinating group efforts and building group-dynamics. Led the team through the research and integration phases of the IMAP protocol without prior experience with the technology",
         techStack: [
@@ -86,6 +67,25 @@ const selectedWorks = [
         beGithub: "https://github.com/Lambda-School-Labs/tagger-be",
         feGithub: "https://github.com/Lambda-School-Labs/tagger-fe",
         linkUrl: "https://tagger-lab.netlify.com/"
+    },
+    {
+        name: "Price My Airbnb",
+        body: "PriceMyAirbnb is a software solution to the problem of properly pricing Airbnb listings. Using a database of United-States AirBnb listings and data science calculations, the app lays out the financial landscape of users’ locations. This allows them to see where they stand and make informed choices on their properties.",
+        techStack: [
+            "React JS",
+            "React-Router",
+            "Axios",
+            "Chart JS",
+            "Auth0",
+            "styled-components",
+            "Node JS",
+            "Express",
+            "Knex",
+            "Postgresql"
+        ],
+        beGithub: "https://github.com/Lambda-School-Labs/airbnb-unit-price-analysis-be",
+        feGithub: "https://github.com/Lambda-School-Labs/airbnb-unit-price-analysis-fe",
+        linkUrl: "https://pricemyairbnb.netlify.com/"
     },
     {
         name: "Med Cabinet",
