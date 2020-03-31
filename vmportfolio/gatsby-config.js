4a21516d9d0e1b256913331a985b050db64bd36e
vladmog <vladmog@gmail.com>
2020-03-30 23:25:52 -0700

    Changed metatag info

--- vmportfolio/gatsby-config.js
+++ vmportfolio/gatsby-config.js
@@ -57,8 +57,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Vladislav Mogilevskiy`,
+        short_name: `Vlad`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
